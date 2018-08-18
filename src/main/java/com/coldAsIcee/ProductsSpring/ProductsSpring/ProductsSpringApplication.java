b09bcb540d97512355ca1ba26211f02c4ccfee3a
ColdAsIcee <wrvd94w@gmail.com>
2018-08-18 15:56:17 +0300

    update

---
+++ src/main/java/com/coldAsIcee/ProductsSpring/ProductsSpring/ProductsSpringApplication.java
@@ -0,0 +1,12 @@
+package com.coldAsIcee.ProductsSpring.ProductsSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsSpringApplication.class, args);
+	}
+}
